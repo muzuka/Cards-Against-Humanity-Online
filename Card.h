53630e1d700e730d39a292608041aadf8dc955aa
muzuka <seanbrown7935@gmail.com>
2013-11-20 16:36:50 -0700

    added documentation

--- Card.h
+++ Card.h
@@ -15,10 +15,10 @@ class Player;
 class Card {
 
   public:
-	std::string content;
-	char type;
-	int numOfAnswers;
-	Player *owner;
+	std::string content;	// card content
+	char type;				// 'b' = black card, 'w' = white card
+	int numOfAnswers;		// number of required white cards
+	Player *owner;			// owner
 
 	Card();
 	Card(std::string, char, int, Player);
